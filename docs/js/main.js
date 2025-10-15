b0c4f6dfa3db1dc0be537caad815885e744d5709
Seppo Tarvainen <tarvainen.seppo@gmail.com>
2025-10-15 10:24:05 +0300

    Close menu when clicked outside.

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,10 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-    // Navigation
+    const nav    = document.querySelector('.navbar');
     const toggle = document.querySelector('.nav-toggle');
     const links = document.querySelector('.nav-links');
-    if (toggle && links) {
-        toggle.addEventListener('click', () => links.classList.toggle('active'));
+
+    const toggleNav = () => {
+        const val = links.classList.toggle('active');
+        toggle.setAttribute('aria-expanded', val ? 'true' : 'false');
     }
+
+    const closeNav = () => {
+        links.classList.remove('active');
+        toggle.setAttribute('aria-expanded', 'false');
+    }
+
+    toggle.addEventListener('click', toggleNav);
+
+    document.addEventListener('click', e => {
+        if (!nav.contains(e.target)) {
+            closeNav();
+        }
+    });
+
+    links.addEventListener('click', (e) => {
+        if (e.target.closest('a')) {
+            closeNav();
+        }
+    });
 
 });
